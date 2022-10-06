b7f2e30c47a913067cded58b69de4e88d23ea57c
PHANAGUS <phanarin03072002@gmail.com>
2022-10-06 15:06:48 +0700

    quiz-04 v1.0

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,9 +1,29 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB } from "../../backendLibs/dbLib";
+
 export default function summaryRoute(req, res) {
   if (req.method === "GET") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
+    const user = checkToken(req);
+    if (!user || !user.isAdmin) {
+      return res.status(403).json({ ok: false, message: "Permission denied" });
+    }
+
     //compute DB summary
-    //return response
+    const all = readUsersDB();
+    const users = all.filter((x) => !x.isAdmin);
+    const admins = all.filter((x) => x.isAdmin);
+    let total = 0;
+    for (let i = 0; i < users.length; i++) {
+      total += users[i].money;
+    }
+
+    return res.json({
+      ok: true,
+      userCount: users.length,
+      adminCount: admins.length,
+      totalMoney: total,
+    });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
